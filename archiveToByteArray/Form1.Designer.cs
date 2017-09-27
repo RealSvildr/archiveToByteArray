e7c31d004a93a25b58fc4b7c3dc27fe8fd303844
Svildr <svildr@gmail.com>
2017-09-27 00:02:16 -0300

    Add files via upload

---
+++ archiveToByteArray/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace archiveToByteArray {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 25);
+            this.input.Name = "input";
+            this.input.ReadOnly = true;
+            this.input.Size = new System.Drawing.Size(341, 20);
+            this.input.TabIndex = 1;
+            this.input.Click += new System.EventHandler(this.input_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::archiveToByteArray.Properties.Resources.Folder;
+            this.button2.Location = new System.Drawing.Point(356, 19);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 26);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 64);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(341, 20);
+            this.output.TabIndex = 4;
+            this.output.Click += new System.EventHandler(this.output_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output";
+            // 
+            // button3
+            // 
+            this.button3.Image = global::archiveToByteArray.Properties.Resources.Folder;
+            this.button3.Location = new System.Drawing.Point(356, 58);
+            this.button3.Margin = new System.Windows.Forms.Padding(0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 26);
+            this.button3.TabIndex = 7;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 122);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Archive to byte array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ archiveToByteArray/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace archiveToByteArray {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            this.MaximizeBox = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            input.Text = getFile("All Files (*.*)|*.*");
+        }
+        private void input_Click(object sender, EventArgs e) {
+            input.Text = getFile("All Files (*.*)|*.*");
+        }
+
+        private void output_Click(object sender, EventArgs e) {
+            output.Text = getFile("Text Files (*.txt)|*.txt");
+        }
+        private void button3_Click(object sender, EventArgs e) {
+            output.Text = getFile("Text Files (*.txt)|*.txt");
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (input.Text == "") { MessageBox.Show("Select an input."); return; }
+            if (output.Text == "") { MessageBox.Show("Select an output."); return; }
+
+            StringBuilder sB = new StringBuilder();
+
+            sB.Append("{ ");
+
+            using (FileStream sR = new FileStream(input.Text, FileMode.Open, FileAccess.Read)) {
+                int bytePerLine = 20;
+                int b = 0, counter = 0;
+
+                while ((b = sR.ReadByte()) > -1) {
+                    sB.Append("0x" + Convert.ToString(b, 16) + ", ");
+
+                    if (counter == bytePerLine) {
+                        sB.Append(Environment.NewLine);
+                        counter = 0;
+                    }
+
+                    counter++;
+                }
+
+                sR.Dispose();
+            }
+
+            sB.Remove(sB.Length - 2, 1);
+            sB.Append("}");
+
+            using (StreamWriter sW = new StreamWriter(output.Text)) {
+                sW.WriteLine(sB.ToString());
+                sW.Dispose();
+            }
+
+            MessageBox.Show("Output has been written.");
+        }
+
+        private string getFile(string filter) {
+            openFileDialog1.Reset();
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.Filter = filter;
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                return openFileDialog1.FileName;
+            }
+
+            return null;
+        }
+    }
+}
